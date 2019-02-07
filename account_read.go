02102b50c0b917ca655001f6d2ddbfb0b13623f8
Leon Bogaert <leon@tim-online.nl>
2019-02-07 16:44:22 +0100

    Added Account/Read action

--- account_read.go
+++ account_read.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/omniboost/go-exactglobe-webservices/odata"
 	"github.com/omniboost/go-exactglobe-webservices/utils"
+	uuid "github.com/satori/go.uuid"
 )
 
 func (c *Client) NewAccountReadRequest() AccountReadRequest {
@@ -29,7 +30,13 @@ type AccountReadRequest struct {
 }
 
 func (c *Client) NewAccountReadQueryParams() *AccountReadQueryParams {
-	return &AccountReadQueryParams{}
+	selectFields, _ := utils.Fields(&Project{})
+	return &AccountReadQueryParams{
+		Select: odata.NewSelect(selectFields),
+		Filter: odata.NewFilter(),
+		Top:    odata.NewTop(),
+		Skip:   odata.NewSkip(),
+	}
 }
 
 type AccountReadQueryParams struct {
@@ -83,8 +90,7 @@ func (s *Client) NewAccountReadRequestBody() AccountReadRequestBody {
 	return AccountReadRequestBody{}
 }
 
-type AccountReadRequestBody struct {
-}
+type AccountReadRequestBody Accounts
 
 func (r *AccountReadRequest) RequestBody() *AccountReadRequestBody {
 	return &r.requestBody
@@ -98,8 +104,7 @@ func (r *AccountReadRequest) NewResponseBody() *AccountReadResponseBody {
 	return &AccountReadResponseBody{}
 }
 
-type AccountReadResponseBody struct {
-}
+type AccountReadResponseBody Accounts
 
 func (r *AccountReadRequest) URL() url.URL {
 	return r.client.GetEndpointURL("Account", r.PathParams())
@@ -121,4 +126,324 @@ func (r *AccountReadRequest) Do() (AccountReadResponseBody, error) {
 	responseBody := r.NewResponseBody()
 	_, err = r.client.Do(req, responseBody)
 	return *responseBody, err
+}
+
+type Accounts []Account
+
+type Account struct {
+	AccountEmployeeId           int
+	AccountEmployeeName         string
+	AccountManagerID            int
+	AccountManagerName          string
+	AccountManagerType          string
+	AddExtraReceiptToOrder      bool
+	AddressID                   string
+	CompanyCode                 string
+	CompanyName                 string
+	AllowBackorder              bool
+	AllowPartialDelivery        bool
+	ApplyShippingCharges        bool
+	ASPDatabase                 string
+	ASPDatabaseServer           string
+	ASPWebsiteServer            string
+	ASPWebsiteURL               string
+	AttachUBL                   bool
+	Attention                   bool
+	AutoAllocate                bool
+	AvalaraEntityUseCode        string
+	BankAccount                 string
+	BankAccountType             string
+	BICCode                     string
+	Blocked                     bool
+	BOELimitAmount              int
+	BOEMaxAmount                int
+	Category_01                 string
+	Category_02                 string
+	Category_03                 string
+	Category_04                 string
+	Category_05                 string
+	Category_06                 string
+	Category_07                 string
+	Category_08                 string
+	Category_09                 string
+	Category_10                 string
+	Category_11                 string
+	Category_12                 string
+	Category_13                 string
+	Category_14                 string
+	Category_15                 string
+	DebtorCreditorAccount       string
+	DebtorAccountName           string
+	ChamberofCommerceNumber     string
+	ChangeVatData               bool
+	ChangeVatServices           bool
+	CheckDate                   *DateTime
+	CityOfLoadUnload            string
+	ClassificationDescription   string
+	ClassificationID            string
+	ContactAccountManager       int
+	ContactCompanyCode          string
+	ContactCreator              int
+	ContactCreatedDate          *DateTime
+	ContactModifiedDate         *DateTime
+	ContactEmail                string
+	ContactFaxNumber            string
+	ContactFirstName            string
+	ContactInitials             string
+	JobDescription              string
+	JobTitle                    string
+	JobTitleDescription         string
+	ContactLanguage             string
+	ContactLanguageDescription  string
+	ContactLastName             string
+	ContactMiddleName           string
+	ContactMobileNumber         string
+	ContactModifier             int
+	ContactPhoneNumber          string
+	Extension                   string
+	PictureFilename             string
+	ContactTitle                string
+	Abbreviation                string
+	TitleDescription            string
+	CiscoDirectory              string
+	CollectionInvoice           string
+	CompanySize                 string
+	SizeDescription             string
+	CompanyType                 int
+	ContactFullName             string
+	ContactID                   uuid.UUID
+	ContactNumberFreeField1     int
+	ContactNumberFreeField2     int
+	ContactNumberFreeField3     int
+	ContactNumberFreeField4     int
+	ContactNumberFreeField5     int
+	CountryOfAssembly           string
+	CountryOfOrigin             string
+	CreditorCode                string
+	CreditorNumber              string
+	Creator                     int
+	CreatorName                 string
+	CreditabilityScenario       string
+	CreditcardNumberReference   string
+	CreditCardDescription       string
+	CreditCardHolder            string
+	CreditLine                  int
+	CreditorNumberReference     string
+	CreditorType                string
+	CurrencyCode                string
+	CurrencyDescription         string
+	AccountCode                 string
+	CodeatSupplier              string
+	Source                      string
+	SourceDescription           string
+	CustomerSince               *DateTime
+	CustomerStatus              string
+	CustomerStatusDescription   string
+	CustomerType                string
+	CustomerTypeDescription     string
+	CreatedDate                 DateTime
+	DateFreeField1              *DateTime
+	DateFreeField2              *DateTime
+	DateFreeField3              *DateTime
+	DateFreeField4              *DateTime
+	DateFreeField5              *DateTime
+	DateLastReminder            *DateTime
+	ModifiedDate                DateTime
+	DebtorCode                  string
+	DebtorNumber                string
+	DebtorNumberReference       string
+	DefaultSelCode              string
+	DefaultSelDescription       string
+	DeliveryAddressline1        string
+	DeliveryAddressline2        string
+	DeliveryAddressline3        string
+	DeliveryCity                string
+	DeliveryCountry             string
+	DeliveryCounty              string
+	DeleteBankAccount           int
+	DeliveryMethod              string
+	DeliverymethodDescription   string
+	DeliveryTerms               string
+	DeliveryPostCode            string
+	DeliveryState               string
+	DestinationCountry          string
+	Directory                   string
+	Discount                    int
+	SupplierID                  uuid.UUID
+	CompanyCodeDebtorAccountID  *uuid.UUID
+	CustomerID                  *uuid.UUID
+	Document                    string
+	DocumentCreato              string
+	DocumentCreatedDate         *DateTime
+	DocumentSubject             string
+	DunBradstreetNumber         string
+	EmailAddress                string
+	ESFContactID                *uuid.UUID
+	ExemptNumber                string
+	CreditCardExpirationDate    string
+	ExtraDuty                   bool
+	FaxNumber                   string
+	FedCategory                 string
+	FedIDNumber                 string
+	FedIDType                   string
+	FiscalCode                  string
+	ContactGender               string
+	GLN                         string
+	GroupDueDates               string
+	GuidFreeField1              *uuid.UUID
+	GuidFreeField2              *uuid.UUID
+	GuidFreeField3              *uuid.UUID
+	GuidFreeField4              *uuid.UUID
+	GuidFreeField5              *uuid.UUID
+	IBAN                        string
+	ID                          string
+	IncoTermAcknowledgeOrder    bool
+	IncoTermCode                string
+	IncoTermConfirmPrices       bool
+	IncoTermProperty            string
+	SectorCode                  string
+	SectorDescription           string
+	IntermediaryassociateID     *uuid.UUID
+	IntrastatSystem             string
+	IntRegion                   string
+	InvoiceAddress              string
+	InvoiceAddress2             string
+	InvoiceAddress3             string
+	InvoiceCity                 string
+	InvoiceCountry              string
+	InvoiceCounty               string
+	InvoiceCopies               int
+	InvoiceDebtor               string
+	InvoiceMethod               string
+	InvoiceAddressPostCode      string
+	InvoiceState                string
+	IsBinaryServiceEnabled      bool
+	IsCommissionable            bool
+	IsTaxExempted               bool
+	ExtraItemDescription        string
+	ExtraDescriptionName        string
+	JournalCode                 string
+	LogoFilename                string
+	MailingAddress1             string
+	MailingAddress2             string
+	MailingAddress3             string
+	Mailbox                     string
+	MailingCity                 string
+	MailingCountry              string
+	MailingCountryDescription   string
+	MailingCounty               string
+	MailingPostcode             string
+	MailingState                string
+	ManagersCostCenter          string
+	ManagersCCDescription       string
+	MessageIDCompanyLogo        string
+	MessageIDRemarks            string
+	Modifier                    int
+	ModifierName                string
+	AccountNamestring           string
+	NumberFreeField1            float64
+	NumberFreeField2            float64
+	NumberFreeField3            float64
+	NumberFreeField4            float64
+	NumberFreeField5            float64
+	IntegerFreeField1           int
+	IntegerFreeField2           int
+	IntegerFreeField3           int
+	IntegerFreeField4           int
+	IntegerFreeField5           int
+	OffsetLedgerAccount         string
+	OffsetAccountName           string
+	Parent                      string
+	ParentCode                  string
+	ParentName                  string
+	PayeeName                   string
+	PaymentCondition            string
+	PaymentConditionDescription string
+	PaymentDay1                 int
+	PaymentDay2                 int
+	PaymentDay3                 int
+	PaymentDay4                 int
+	PaymentDay5                 int
+	PhoneNumber                 string
+	PhoneExtension              string
+	PhoneQueue                  string
+	PostalAddressLine1          string
+	PostalAddressLine2          string
+	PostalAddressLine3          string
+	PostalCity                  string
+	PostalCountry               string
+	PostalCounty                string
+	PostalPostCode              string
+	PostalState                 string
+	PriceList                   string
+	PriceListDescription        string
+	PurchaseOrderConfirmation   bool
+	OrderConfirmation           string
+	Rating                      string
+	RatingDescription           string
+	Reminder                    bool
+	ResellerCode                string
+	ResellerID                  *uuid.UUID
+	ResellerName                string
+	SecurityLevel               int
+	SignDate                    *DateTime
+	MailingStateDescription     string
+	StatementDate               *DateTime
+	StatementFrequency          string
+	StatementNumber             int
+	StatementPrint              bool
+	StatFactor                  string
+	AccountStatus               string
+	SubSector                   string
+	SubSectorDescription        string
+	SyncID                      *uuid.UUID
+	Terms                       string
+	TerritoryCode               string
+	TextFreeField1              string
+	TextFreeField10             string
+	TextFreeField11             string
+	TextFreeField12             string
+	TextFreeField13             string
+	TextFreeField14             string
+	TextFreeField15             string
+	TextFreeField16             string
+	TextFreeField17             string
+	TextFreeField18             string
+	TextFreeField19             string
+	TextFreeField2              string
+	TextFreeField20             string
+	TextFreeField21             string
+	TextFreeField22             string
+	TextFreeField23             string
+	TextFreeField24             string
+	TextFreeField25             string
+	TextFreeField26             string
+	TextFreeField27             string
+	TextFreeField28             string
+	TextFreeField29             string
+	TextFreeField3              string
+	TextFreeField30             string
+	TextFreeField4              string
+	TextFreeField5              string
+	TextFreeField6              string
+	TextFreeField7              string
+	TextFreeField8              string
+	TextFreeField9              string
+	TransactionA                string
+	TransactionB                string
+	Transport                   string
+	Transshipment               string
+	TriangularCountry           string
+	TypeSince                   *DateTime
+	UseAvalaraTaxation          bool
+	VatCode                     string
+	VATLiability                string
+	VATNumber                   string
+	VatServices                 string
+	WebAddress                  string
+	YesNoFreeField1             bool
+	YesNoFreeField2             bool
+	YesNoFreeField3             bool
+	YesNoFreeField4             bool
+	YesNoFreeField5             bool
 }
--- account_read_test.go
+++ account_read_test.go
@@ -1,1 +1,45 @@
 package webservices_test
+
+import (
+	"encoding/json"
+	"log"
+	"net/url"
+	"os"
+	"testing"
+
+	_ "github.com/joho/godotenv/autoload"
+	webservices "github.com/omniboost/go-exactglobe-webservices"
+)
+
+func TestAccountRead(t *testing.T) {
+	baseURLString := os.Getenv("GLOBE_BASE_URL")
+	baseURL, err := url.Parse(baseURLString)
+	if err != nil {
+		t.Error(err)
+	}
+	databaseName := os.Getenv("GLOBE_DATABASE_NAME")
+	databaseServerName := os.Getenv("GLOBE_DATABASE_SERVER_NAME")
+	username := os.Getenv("GLOBE_USERNAME")
+	password := os.Getenv("GLOBE_PASSWORD")
+	log.Println(baseURLString, databaseName, username, password)
+
+	client := webservices.NewClient(nil, *baseURL, databaseName, databaseServerName, username, password)
+	client.SetDebug(true)
+	client.SetDisallowUnknownFields(true)
+
+	req := client.NewAccountReadRequest()
+	req.QueryParams().Top.Set(1)
+	// req.QueryParams().Select.Add("CompanyName")
+	// req.QueryParams().Select.Add("FirstName")
+	// req.QueryParams().Select.Add("LastName")
+	// req.QueryParams().Select.Add("FullName")
+
+	// req.PathParams().Date = time.Date(2018, 12, 5, 0, 0, 0, 0, time.UTC)
+	resp, err := req.Do()
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := json.MarshalIndent(resp, "", "  ")
+	log.Println(string(b))
+}
